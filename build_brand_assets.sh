fc362fd564cd00fb49545d3344c5a5c3b3ca5438
1AhmadEgy <114110463+1AhmadEgy@users.noreply.github.com>
2026-09-11 06:31:27 +0200

    brand: fix transparent logo generation and canonical asset exports

--- build_brand_assets.sh
+++ build_brand_assets.sh
@@ -6,7 +6,8 @@ mkdir -p "$ASSETS_DIR"
 
 command -v inkscape >/dev/null 2>&1 || { echo "Inkscape is required."; exit 1; }
 
-inkscape "$ASSETS_DIR/logo.svg" --export-type=png --export-filename="$ASSETS_DIR/logo_transparent.png" -w 2000 -h 2000
+inkscape "$ASSETS_DIR/logo.svg" --export-type=png --export-filename="$ASSETS_DIR/logo.png" -w 2000 -h 2000
+inkscape "$ASSETS_DIR/logo_transparent.svg" --export-type=png --export-filename="$ASSETS_DIR/logo_transparent.png" -w 2000 -h 2000
 inkscape "$ASSETS_DIR/icon.svg" --export-type=png --export-filename="$ASSETS_DIR/avatar.png" -w 800 -h 800
 inkscape "$ASSETS_DIR/watermark.svg" --export-type=png --export-filename="$ASSETS_DIR/watermark.png" -w 512 -h 512
 inkscape "$ASSETS_DIR/thumbnail_template.svg" --export-type=png --export-filename="$ASSETS_DIR/thumbnail_template.png" -w 1280 -h 720
